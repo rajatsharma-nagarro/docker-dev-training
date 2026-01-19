fe438a013a7a10aa2c7799e51ba7505992a35606
rajatsharma04 <rajat.sharma04@nagarro.com>
2026-01-19 16:02:37 +0530

    Initial Maven project setup

---
+++ docker-dev-training/src/test/java/com/docker/dev/training/DockerDevTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.dev.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDevTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
