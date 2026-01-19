02c5e17ac887f5f667066e3247f92db70e28400a
rajatsharma-nagarro <rajat.sharma04@nagarro.com>
2026-01-19 16:05:01 +0530

    Initial Maven project setup (#1)

---
+++ docker-dev-training/src/main/java/com/docker/dev/training/DockerDevTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.docker.dev.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDevTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDevTrainingApplication.class, args);
+    }
+
+}
